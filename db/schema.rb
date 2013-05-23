56e3851f2ca58164291b566dd7ea65e4677627be
Vincent Chu <c.tnecniv@gmail.com>
2013-05-23 13:12:38 -0700

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(:version => 20130522223812) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",        :default => 0
     t.datetime "locked_at"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
